10ccc780a7ecc76fa4fa485da46a49031b37f10c
SeongChoi <choies921003@gmail.com>
2020-04-09 15:41:37 -0700

    Added save, reset and button for search photo and display image. Also added loadInputHandler function handles whatever image I pick it will display.

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-
+  /*
+  loadInputHandler function will handle whatever image I pick will get that image location and setting attribute to src image tag that image I picked. 
+  */
+ const loadImage = document.getElementById("find-file");
+ const loadInputHandler = e => {
+   const imageFile = e.target.files[0];
+   const imageElement = document.getElementById("image");
+   imageElement.setAttribute("src", URL.createObjectURL(imageFile));
+ };
+ loadImage.onchange = loadInputHandler;
+ 
 }, false);
